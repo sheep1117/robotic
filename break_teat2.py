1eb174ea3ecc5b84f1b647abdc0387e2e81efef7
­정현우 <sheep17@hanyang.ac.kr>
2022-04-05 13:08:04 +0000

    test1

---
+++ break_teat2.py
@@ -0,0 +1,9 @@
+while(Ture):
+    num=int(input(" 정수 입력 해주세요: "))
+    if num==-1:
+        print("stop")
+        break
+    if num%2==0:
+        print(num,"is even")
+    else:
+        print(num,"is odd")
---
+++ break_test.py
@@ -0,0 +1,5 @@
+while(True)
+num=int(input("input the number"))
+if num==-1:
+    print("stop")
+    break
