af1606e2e1baf8cabc6ca109092156e26fc96451
­정현우 <sheep17@hanyang.ac.kr>
2022-04-05 07:09:50 +0000

    test

---
+++ continue_test1.py
@@ -0,0 +1,5 @@
+num=int(input('number: '))
+for i in range(1,n+1):
+    if i % 3 !=0:
+        continue
+print(i,': 3의 배수 ')
---
+++ for_test.py
@@ -0,0 +1,3 @@
+num=int(input("enter the repeat number"))
+for i in range(num,4,num):
+    print("you are so go student")
